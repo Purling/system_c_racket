4a06be13f568379d5d3553bd69b53cdb7c9e3653
u7312578 <u7312578@anu.edu.au>
2024-07-03 21:14:48 +1000

    Added comments from meeting with Fabian

--- System_C.rkt
+++ System_C.rkt
@@ -6,8 +6,7 @@
 ;; Grammar
 (define-language System_C
   (e x
-     0
-     1
+     natural
      true
      false
      (box b))
@@ -21,7 +20,7 @@
      (b (e ... #\, b ...)) ;; QUESTION: Are the two 'b's present different? If not, I will probably need an underscore or something to differentiate
      (val x = s #\; s)
      (return e)
-     (try f ⇒ s with (x ... #\, k) ⇒ s) ;; QUESTION: Do the ellipses only apply to x
+     (try f ⇒ s with (x ... #\, k) ⇒ s)
      (l s with (x ... k) ⇒ s ))
   
   (τ Int
@@ -38,13 +37,22 @@
      (f :* σ)
      (f : C σ))
 
+  (c none
+     C)
+
   (x variable-not-otherwise-mentioned)
 
   (f variable-not-otherwise-mentioned)
 
+  (k variable-not-otherwise-mentioned)
+
+  ;; TODO: Use either gensym or fresh to generate unique runtime labels (this would be for try blocks). If using gensym, use pattern for defining l in the grammar otherwise, fresh will require something else
+
+  ;; QUESTION: Not sure the parentheses need to be there for the x ... k. Don't think they actually add anything of real substance
+  ;; TODO: Think about the run-time label l and the continuation k
   (E ::= hole
-     (#\{ val x = E #\; s #\})
-     (l #\{ E #\} with #\{ (x ... k) => s #\})) ;; QUESTION: Not sure the parentheses need to be there for the x ... k. Don't  think they actually add anything of real substance
+     (val x = E #\; s)
+     (l E with (x ... #\, k) ⇒ s))
   )
 
 ;; Metafunction which attempts to find an element within a list and either returns #f or the element found
@@ -102,8 +110,10 @@
   )
 
 ;; Subset metafunction
+;; TODO: Extend this so that the 'superset' is 'unknown'
+;; TODO: Put as where clause in block typing and statement typing
 (define-metafunction System_C
-  subset : C C -> C         ;; TODO: Look into if this has to be a maybe type (optional type) or something similar for the output. The output should be a bool, but we have yet to define a #f and #t in our language.
+  subset : C C -> boolean
   [(subset (f f_1 ...) (f_2 ... f f_4 ...))
    (subset (f_1 ...) (f_2 ... f f_4 ...))
 
@@ -147,24 +157,35 @@
    #f])
 
 ;; TODO: Figure out how exactly substitution would work
+;; TODO: Make sure that the input and output is correct and works correctly
 
 ;; Typing rules for block typing
 (define-judgment-form System_C
-  #:contract (block-type Γ b σ C C) ;; QUESTION: Is this the way in which we would do bi-directional input and output? I have done so naively.
-  #:mode (block-type I I I I O)
+  #:contract (block-type Γ b σ c C)
+  #:mode (block-type I I O I O)
 
-  [(where g (find (f : C σ) Γ))
+  ;; TODO: Change the find metafunction such that it returns either a found type σ or a default value (probably something like #f)
+  ;; N.B.: The find metafunction needs to be able to return a value which has distinction between the different types of g's (i.e., the difference between (x : τ), (f :* σ) and (f : C σ)
+  ;; TODO: Create a new grammar construct which enables a contract for find (i.e., a new construct which is (C σ), (* σ) and τ
+  [(where (C σ) (find f Γ))
    ---------------------------- "Transparent"
-   (block-type Γ f σ C C)]
+   (block-type Γ f σ c C)]
 
-  [(where g (find (f :* σ) Γ))
+  ;; QUESTION: Not sure if the (f) need to be '(f) (i.e., taken out)
+  [(where (* σ) (find f Γ))
    --------------------------- "Tracked"
-   (block-type Γ f σ (f) (f))] ;; QUESTION: Not sure if the (f) need to be '(f) (i.e., taken out)
+   (block-type Γ f σ (f) (f))]
 
   ;; QUESTION: Is this the right way to express the terms with arrows on top?
-  [(statement-type (Γ ((x : τ_i) ...) ((f :* σ) ...)) s τ (C (f ...)) (C (f ...)))
-   --------------------------------------------------------------------------------------------- "Block"
-   (block-type Γ (((x : τ_i) ...) #\, ((f : σ) ...) ⇒ s) ((τ_i ...) #\, ((f : σ) ...) → τ) C C)] ;; TODO: Check if I need something to distinguish the many different tau's. Check if the parentheses are necessary around the (x : τ_i) ... and (f : σ) ...)
+  ;; SANITY-CHECK: Confirm that g_j is an f
+  ;; QUESTION: Is there a correct way in which I should be expressing the τ_i's? Is essence, each i is an identifier for a specific τ, so would it matter?
+  ;; TODO: Check if I need something to distinguish the many different τ's. Check if the parentheses are necessary around the ((x : τ_i) ...) and ((f : σ) ...)
+  ;; TODO: Create a 'c' append which if given a #f, just returns a #f otherwise it returns the appended
+  ;; TODO: Make sure that the output 'C' has all f's removed
+  ;; TODO: Make sure that the result of the appending is a superset using the subset function
+  ;[(statement-type (Γ ((x : τ_i) ...) ((f :* σ) ...)) s τ (C f ...) (C f ...))
+  ; --------------------------------------------------------------------------------------------- "Block"
+  ; (block-type Γ (((x : τ_i) ...) #\, ((f : σ) ...) ⇒ s) ((τ_i ...) #\, ((f : σ) ...) → τ) c C)]
 
   [(expr-type Γ e (σ at C))
    ----------------------------------------- "BoxElim"
@@ -173,50 +194,50 @@
 
 ;; Typing rule for expression typing
 (define-judgment-form System_C
-  #:mode (expr-type I I I) ;; QUESTION: I think that the mode for expr-type can just be all inputs
+  #:mode (expr-type I I O)
   #:contract (expr-type Γ e τ)
-  
-  [(where #t (exact-integer? e)) ;; QUESTION: We may have to escape to Racket to perform this operation?
-   ------------------------------ "Lit"
-   (expr-type Γ e Int)]
 
-  [(where g (find (x : τ) Γ))
+  [
+   ------------------------------ "Lit"
+   (expr-type Γ natural Int)]
+
+  ;; N.B.: We should treat Γ as a lookup dictionary and use the key x to find τ
+  ;; TODO: Change the find metafunction such that it returns either a found type τ or a default value (probably something like #f)
+  [(where τ (find x Γ))
    -------------------------- "Var"
    (expr-type Γ x τ)]
 
-  [(block-type Γ b σ C C)
+  ;; TODO: Create a special value which indicates that we don't know what the C value is
+  [(block-type Γ b σ none C)
    ------------------------------- "BoxIntro"
    (expr-type Γ (box b) (σ at C))]
   
   )
 
-;; TODO: Define substitution for statement typing
+;; TODO: Define subset for statement typing
 
 (define-judgment-form System_C
-  #:mode (statement-type I I I I O)
-  #:contract (statement-type Γ σ τ C C)
+  #:mode (statement-type I I O I O)
+  #:contract (statement-type Γ σ τ c C)
 
-  ;; TODO: Create an append metafunction for sets
-  ;; TODO: Fix problem with unbound variable
-  ;; QUESTION: For the union, can we just call the metafunction which does a set union between the two sets given?
-  ;[(statement-type Γ s_0 τ_0 C_0 C_0)
-  ; (statement-type (Γ (x : τ_0)) s_1 τ_1 C_1 C_1)
-  ; --------------------------------------------------------------------------- "Val"
-  ; (statement-type Γ (val x = s_0 #\; s_1) τ_1 (set-append C_0 C_1) (set-append C_0 C_1))]
+  ;; QUESTION: Is the 'none' correct here? My logic is that, we are getting the C_0 and C_1 from the two statement-type's and these are then given to the output (set-append C_0 C_1)
+  [(statement-type Γ s_0 τ_0 none C_0)
+   (statement-type (Γ (x : τ_0)) s_1 τ_1 none C_1)
+   ----------------------------------------------------------------------- "Val"
+   (statement-type Γ (val x = s_0 #\; s_1) τ_1 none (set-append C_0 C_1))]
 
   ;; TODO: Make sure that the emptyset is expressed correctly as a null set. (i.e., I don't think it should be expressed as \emptyset. Instead, it should probably be an empty list)
   [(expr-type Γ e τ)
    ---------------------------------------------------- "Ret"
    (statement-type Γ (return e) τ \emptyset \emptyset)]
 
-  ;; QUESTION: How do I express the multiple (expr-type Γ e_i τ_i) and (block-type b_j σ_j C_j)?
-  ;; TODO: Figure out the substitution in App for (τ[f_j→C_j])
-  ;; RESOLVE: Once again, input and output needs to be resolved
-  ;[(block-type Γ b ((τ_i ...) #\, ((f : σ) ...) → τ) C C)
-  ; (expr-type Γ e_i τ_i)
-  ; (block-type Γ b_j σ_j C_j C_j)
-  ;  ----------------------------------------------------- "App"
-  ; (statement-type Γ (b (e_i ... #\, b_j ...)) (τ) (set-append C (C_j ...)) (set-append C (C_j ...)))]
+  ;; QUESTION: Have I expressed the multiple of the (expr-type Γ e_i τ_i) and (block-type Γ b_j σ_j C_j C_j) correctly?
+  ;; TODO: Figure out the substitution in App for (τ[f_j→C_j]). This will probably just be a redex metafunction or something of the sort.
+  [(block-type Γ b (τ_i ... #\, (f : σ) ... → τ) C C)
+   (expr-type Γ e_i τ_i) ...
+   (block-type Γ b_j σ_j C_j C_j) ...
+    -------------------------------------------------------------------------------------------------- "App"
+   (statement-type Γ (b (e_i ... #\, b_j ...)) (τ) (set-append C (C_j ...)) (set-append C (C_j ...)))]
 
   ;; TODO: Confirm logic around the input and output for def
   ;[(block-type Γ b σ C C_prime)  ;; QUESTION: Confirm logic of the output and input in this instance. (i.e., the typing rule is given C and we output a C_prime)
@@ -233,8 +254,9 @@
   )
 
 ;; Reduction Rules
-(define red
-  (reduction-relation System_C ; I sometimes see a domain tag, I am not sure what exactly it is referring to or what it means
+(define reduction
+  ;; TODO: Determine if a 'domain' tag is necessary
+  (reduction-relation System_C
 
    ;(--> (in-hole E (unbox box b)) ; Add in-hole. But, it's complaining that the in-hole may match more than one context at once
    ;     (b))
@@ -248,7 +270,7 @@
    (--> (def f = w #\; s)
         (substitute s [f w])) ; Make sure that the substitute parameters are given the right way (i.e., [f w] or [w f])
 
-   (--> (l #\{ return v #\} with h)
+   (--> (l (return v) with h)
         (v))
 
    ;(--> (#\{ (x ... f ...) => s #\} (v ... w ...))
